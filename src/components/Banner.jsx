1dee9144356fffa033645125d037e0a987ebc71e
GabrielAsis <asisgabriel27@gmail.com>
2024-07-18 12:27:38 +0400

    Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-
 function Banner() {
   return (
     <>
-      <div className='flex justify-center items-center'>
-        <h1 className='text-3xl text-center'>Dictionary</h1>
+      <div className='flex relative justify-center items-center bg-green h-[37vh]'>
+        <div className='absolute inset-0 bg-no-repeat bg-cover bg-dictionary-pattern'></div>
+        <h1 className='z-10 text-7xl text-center text-white font-righteous'>Dictionary</h1>
       </div>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: '#49796B',
+        darkGreen: '#013220',
+        lightGreen: '#98BCB1',
+      },
+      fontFamily: {
+        righteous: ['Righteous', 'sans-serif'],
+        poppins: ['Poppins', 'serif'],
+        inter: ['Inter', 'serif'],
+        neuton: ['Neuton', 'serif']
+      },
+      backgroundImage: {
+        'dictionary-pattern': "url('/src/assets/pattern background.png')"
+        
+      },
+      backgroundSize: {
+        'customSize': '2rem 2rem',
+      },
+    },
   },
   plugins: [],
 }
